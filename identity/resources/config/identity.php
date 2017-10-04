b8542d0c66e05cc1073e0e481626bd50a4655960
40Enissay <yassine.ouachhal@gmail.com>
2017-10-04 20:51:02 +0100

    add configuration file that specify the strategy used to handle authenticated requests

---
+++ identity/resources/config/identity.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ouachhal
+ * Date: 16/08/2017
+ * Time: 19:28
+ */
+
+return [
+
+    "users" => [
+        "subject" => [
+            "hosted" => true,
+            'force-create' => true,
+        ],
+        "claims" => [
+            "username" => "username",
+            "phone" => "phone",
+            "scopes" => "scopes"
+        ]
+    ],
+    "clients" => [
+        "claims" => [
+            "scopes" => "scopes"
+        ]
+    ]
+
+];
